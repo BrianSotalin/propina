dbe3ab80dd70956724161e4654036b2e666d78d2
Brian Sotalin <dannysotalin@gmail.com>
2022-01-07 15:23:05 -0500

    dark-mode

--- app.js
+++ app.js
@@ -11,3 +11,26 @@ input.style.setProperty("--value", input.value);
 input.addEventListener("input", function(evt) {
    input.style.setProperty("--value", input.value);
   },false);
+  const btnSwitch = document.querySelector('#switch');
+
+btnSwitch.addEventListener('click', () => {
+	document.body.classList.toggle('dark');//toggle agrega una clase si no tiene y la elimina si ya la tiene
+	btnSwitch.classList.toggle('active');
+});
+
+function downloadImage(url, name){
+	fetch(url)
+	  .then(resp => resp.blob())
+	  .then(blob => {
+		  const url = window.URL.createObjectURL(blob);
+		  const a = document.createElement('a');
+		  a.style.display = 'none';
+		  a.href = url;
+		  // the filename you want
+		  a.download = name;
+		  document.body.appendChild(a);
+		  a.click();
+		  window.URL.revokeObjectURL(url);
+	  })
+	  .catch(() => alert('Lo siento mi rey ocurrio un error F'));
+}
